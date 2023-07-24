fca01cc097b937d32af1b4a6e3aadea126e13407
Itsprime <itsprimesuarez@gmail.com>
2023-07-23 22:25:01 -0700

    added draggable feature
    with unable to drag new added todos

--- script.js
+++ script.js
@@ -10,19 +10,28 @@ submit.addEventListener('click', e => {
 
 //create a div that is inside the div(input_text) and button ? and display: grid on css
 //create an eventlistener, when click on delete button, delete whole div. 
+
+// PROBLEM:
+// newly created Todos dont get moved
+// 
+
+let draggables = document.querySelectorAll('.wrapperDiv');
 function createTODO(amount) {
     var createWrapperDiv = document.createElement("div");
-    LIST.appendChild(createWrapperDiv)
     createWrapperDiv.setAttribute(`id`, `${amount}`)
     createWrapperDiv.setAttribute(`class`, `wrapperDiv`)
- 
+    createWrapperDiv.setAttribute(`draggable`, `true`)
+    LIST.appendChild(createWrapperDiv)
+
+    draggables = document.querySelectorAll('.wrapperDiv');
+
     const input_text = document.createTextNode(document.getElementById("text-input").value);
     if (input_text.textContent == "") return alert("No text - Please add text");
     var createDiv = document.createElement("div");
     createDiv.append(input_text)
     createDiv.setAttribute(`id`, `${amount}`)
     createWrapperDiv.appendChild(createDiv)
-    
+
     //creates DELETE button
     var createButton = document.createElement("button");
     createButton.setAttribute(`id`, `${amount}`)
@@ -36,7 +45,37 @@ function createTODO(amount) {
         button.addEventListener("click", deleteTODO));
 }
 
-function deleteTODO(event){
+// let draggables = document.querySelectorAll('.wrapperDiv');
+let containers = document.querySelectorAll('.container');
+console.log(draggables)
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+        console.log("dragging atm ")
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+        console.log("dragging stop ")
+    })
+})
+
+//check or idk can be when you drag the class gets set to dragging? and the created ones arent set to dragging ever
+//so the dragging and dragging stop arent working for the created divs, how to solve this???
+
+//when I hover new todo, it doesnt do any of the dragging in console messages
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const draggable = document.querySelector('.dragging')
+        container.append(draggable)
+        console.log(draggable)
+    })
+})
+
+function deleteTODO(event) {
     var elem = document.getElementById(this.id);
     if (this.id == "submit-button") return console.log("ignoring submit-button")
     else return elem.parentNode.removeChild(elem) + console.log(this.id);
